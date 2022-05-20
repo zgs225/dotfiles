f054dedca80602e03e267814fb4a6de18f45c4b3
zgs225 <zgs225@gmail.com>
2022-05-20 11:38:58 +0800

    update vim config

--- antigen.zsh
+++ antigen.zsh
@@ -3,7 +3,9 @@ source /usr/local/Cellar/antigen/2.2.3/share/antigen/antigen.zsh
 antigen use oh-my-zsh
 
 antigen bundle z
+antigen bundle git
 antigen bundle command-not-found
+antigen bundle zsh-users/zsh-autosuggestions
 
 antigen bundle zsh-users/zsh-syntax-highlighting
 
