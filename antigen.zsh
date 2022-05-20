1aca71386d6118a2a576421beb9535e6e18e5902
zgs225 <zgs225@gmail.com>
2022-05-20 10:23:51 +0800

    updated

--- antigen.zsh
+++ antigen.zsh
@@ -3,7 +3,10 @@ source /usr/local/Cellar/antigen/2.2.3/share/antigen/antigen.zsh
 antigen use oh-my-zsh
 
 antigen bundle z
+antigen bundle command-not-found
 
 antigen bundle zsh-users/zsh-syntax-highlighting
+
+antigen theme robbyrussell
 
 antigen apply
