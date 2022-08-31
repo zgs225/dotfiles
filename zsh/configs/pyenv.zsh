f55ba3471726e1a2f76b4625643de078862f5172
zgs225 <zgs225@gmail.com>
2022-08-31 11:27:27 +0800

    chore(zsh): do something at zsh

--- zsh/configs/pyenv.zsh
+++ zsh/configs/pyenv.zsh
@@ -1,3 +1,6 @@
-export PYENV_ROOT="$HOME/.pyenv"
-command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+command -v pyenv > /dev/null
+if [ ! $? ]; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
+fi
