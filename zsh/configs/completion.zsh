9f5e7e82b86c0e8094416353c3956ef63a6a7450
zgs225 <zgs225@gmail.com>
2023-06-30 11:24:53 +0800

    add argocd completion config

--- zsh/configs/completion.zsh
+++ zsh/configs/completion.zsh
@@ -4,3 +4,8 @@ compinit
 if command -v kubectl &> /dev/null; then
   source <(kubectl completion zsh)
 fi
+
+if command -v argocd &> /dev/null; then
+  source <(argocd completion zsh)
+  compdef _argocd argocd
+fi
