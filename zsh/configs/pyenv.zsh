0e1a88794f912c30b1e859fc44b2a3cc39138f18
zgs225 <zgs225@gmail.com>
2024-06-27 14:11:56 +0800

    update vim config

--- zsh/configs/pyenv.zsh
+++ zsh/configs/pyenv.zsh
@@ -1,6 +1,9 @@
-command -v pyenv > /dev/null
-if [ ! $? ]; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
+if command -v pyenv &> /dev/null; then
+    export PYENV_ROOT="$HOME/.pyenv"
+    [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init -)"
+fi
+
+if command -v pyenv-virtualenv &> /dev/null; then
+    eval "$(pyenv virtualenv-init -)"
 fi
