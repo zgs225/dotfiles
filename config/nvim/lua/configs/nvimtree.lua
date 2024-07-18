fcd7df309f515ad67daf2e8eb006f28ef786907b
zgs225 <zgs225@gmail.com>
2024-07-18 19:29:05 +0800

    优化 NvimTree 与 Outline 插件配置

--- config/nvim/lua/configs/nvimtree.lua
+++ config/nvim/lua/configs/nvimtree.lua
@@ -17,4 +17,18 @@ M.on_attach = function(bufnr)
   map("n", "?", api.tree.toggle_help, opts "Help")
 end
 
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd({ "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("NvimTreePost", { clear = true }),
+  callback = function(args)
+    local win_n = #vim.api.nvim_tabpage_list_wins(0)
+    local buf_name = vim.api.nvim_buf_get_name(args.buf) ---@type string
+    local pattern = "^.*NvimTree_%d+$"
+    if win_n == 1 and buf_name:match(pattern) ~= nil then
+      vim.cmd "q"
+    end
+  end,
+})
+
 return M
--- config/nvim/lua/configs/outline.lua
+++ config/nvim/lua/configs/outline.lua
@@ -6,9 +6,9 @@ autocmd({ "BufEnter" }, {
   group = vim.api.nvim_create_augroup("Outline", { clear = true }),
   callback = function(args)
     local buf_name = vim.api.nvim_buf_get_name(args.buf) ---@type string
-    local suffix = "OUTLINE_1"
-    local tab_n = #vim.api.nvim_tabpage_list_wins(0)
-    if tab_n == 1 and buf_name:sub(-#suffix) == suffix then
+    local pattern = "^.*OUTLINE_%d+"
+    local win_n = #vim.api.nvim_tabpage_list_wins(0)
+    if win_n == 1 and buf_name:match(pattern) ~= nil then
       vim.cmd "quit"
     end
   end,
