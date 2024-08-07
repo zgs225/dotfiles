ed3edbd2a7171b130adff295c7dedb48bb37e93c
zgs225 <zgs225@gmail.com>
2024-08-07 10:55:40 +0800

    fix: golang path variable; stty setup

--- zsh/configs/go.zsh
+++ zsh/configs/go.zsh
@@ -1,2 +1,9 @@
-export PATH="$PATH:/usr/local/go/bin"
+GO_DIRS=("/usr/local/go", "/opt/go")
+
+for DIR in $GO_DIRS[@]; do
+  if [ -d "${DIR}" ]; then
+    export PATH="${PATH}:${DIR}/bin"
+  fi
+done
+
 export PATH="$PATH:$(go env GOPATH)/bin"
