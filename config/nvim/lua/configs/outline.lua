57e7f444ebbcc2cd4e9ba97634669432506a8918
zgs225 <zgs225@gmail.com>
2024-08-09 16:19:06 +0800

    [nvim] add new local buf map for nvimtree panel

--- config/nvim/lua/configs/outline.lua
+++ config/nvim/lua/configs/outline.lua
@@ -1,15 +1,1 @@
 require("outline").setup {}
-
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd({ "BufEnter" }, {
-  group = vim.api.nvim_create_augroup("Outline", { clear = true }),
-  callback = function(args)
-    local buf_name = vim.api.nvim_buf_get_name(args.buf) ---@type string
-    local pattern = "^.*OUTLINE_%d+"
-    local win_n = #vim.api.nvim_tabpage_list_wins(0)
-    if win_n == 1 and buf_name:match(pattern) ~= nil then
-      vim.cmd "quit"
-    end
-  end,
-})
