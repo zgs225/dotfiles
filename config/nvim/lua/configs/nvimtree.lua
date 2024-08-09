3df166a215911bae1c074e91078fd55eff33f32a
zgs225 <zgs225@gmail.com>
2024-08-09 16:19:06 +0800

    [nvim] add new local buf map for nvimtree panel

--- config/nvim/lua/configs/nvimtree.lua
+++ config/nvim/lua/configs/nvimtree.lua
@@ -14,21 +14,8 @@ M.on_attach = function(bufnr)
   map("n", "s", api.node.open.vertical, opts "Open: Vertical Split")
   map("n", "i", api.node.open.horizontal, opts "Open: Horizontal Split")
   map("n", "x", api.node.navigate.parent_close, opts "Close Parent Folder")
+  map("n", "t", api.node.open.tab, opts "Open: In New Tab")
   map("n", "?", api.tree.toggle_help, opts "Help")
 end
-
-local autocmd = vim.api.nvim_create_autocmd
-
-autocmd({ "BufEnter" }, {
-  group = vim.api.nvim_create_augroup("NvimTreePost", { clear = true }),
-  callback = function(args)
-    local win_n = #vim.api.nvim_tabpage_list_wins(0)
-    local buf_name = vim.api.nvim_buf_get_name(args.buf) ---@type string
-    local pattern = "^.*NvimTree_%d+$"
-    if win_n == 1 and buf_name:match(pattern) ~= nil then
-      vim.cmd "q"
-    end
-  end,
-})
 
 return M
