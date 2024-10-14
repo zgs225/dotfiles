d50a3bf89fdbdb2a550d585dac80624ee032a052
zgs225 <zgs225@gmail.com>
2024-10-14 20:46:01 +0800

    [neovim] add plugin to pretty diagonistic display

--- config/nvim/lua/plugins/devtools.lua
+++ config/nvim/lua/plugins/devtools.lua
@@ -82,4 +82,14 @@ return {
       require "configs.neotest"
     end,
   },
+
+  -- 代码诊断展示
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "User FilePost",
+    config = function()
+      vim.diagnostic.config { virtual_text = false }
+      require("tiny-inline-diagnostic").setup()
+    end,
+  },
 }
