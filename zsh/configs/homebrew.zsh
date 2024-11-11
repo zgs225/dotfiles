39e82a5aae84a9fdd9450a725001308b94e734b7
zgs225 <zgs225@gmail.com>
2024-11-11 13:57:39 +0800

    fix homebrew on macos

--- zsh/configs/homebrew.zsh
+++ zsh/configs/homebrew.zsh
@@ -8,3 +8,9 @@ export PATH="/usr/local/bin:$PATH"
 if [[ -f /home/linuxbrew/.linuxbrew/bin/brew ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
+
+# For macos homebrew
+if [[ -f /opt/homebrew/bin/brew ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
