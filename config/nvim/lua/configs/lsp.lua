dcd0dd688a1e197e29348ba199dc828d169ead5e
zgs225 <zgs225@gmail.com>
2025-01-07 15:30:48 +0800

    modify neovim config

--- config/nvim/lua/configs/lsp.lua
+++ config/nvim/lua/configs/lsp.lua
@@ -50,9 +50,9 @@ for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = function(client, bufnr)
       on_attach(client, bufnr)
-      vim.lsp.inlay_hint.enable(true)
       -- use plugin to display diagnostic messages
       vim.diagnostic.config { virtual_text = false }
+      vim.lsp.inlay_hint.enable(true)
     end,
     on_init = on_init,
     capabilities = capabilities,
