08e95eeb7e1f09020752a466a0e92bd5516fd734
zgs225 <zgs225@gmail.com>
2025-08-25 15:44:02 +0800

    feat: optimize neovim config for Makefile

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -35,3 +35,15 @@ vim.filetype.add {
 if vim.fn.exists "&messagesopt" == 1 then
   o.messagesopt = "wait:150,history:500"
 end
+
+-- Makefile 特定设置
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "make",
+  callback = function()
+    vim.opt_local.expandtab = false
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.list = true
+    vim.opt_local.listchars = { tab = "→ ", trail = "·" }
+  end,
+})
