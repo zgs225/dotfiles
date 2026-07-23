1638a2e492679c55ceeccf0ae333df4ec28c8cbd
yuez <i@yuez.me>
2026-07-23 19:32:05 +0800

    feat(nvim): <C-g> leader in pi chat buffers + telescope vim.ui.select
    - <C-g>s resume session: pi's vim.ui.select routed through telescope
      via telescope-ui-select (dropdown theme); also upgrades LSP code
      actions and every other vim.ui.select caller
    - <C-g>n new session, <C-g>m pick from curated model list
      (k3 / kimi-for-coding / deepseek-v4-pro / qwen3.8-max-preview)
    - mappings buffer-local to pi-chat-* filetypes, n+i modes

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -23,6 +23,22 @@ return {
   },
 
   {
+    -- Route vim.ui.select through telescope (dropdown theme). Benefits
+    -- pi.nvim's :PiResume session picker, LSP code actions, etc.
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-telescope/telescope-ui-select.nvim" },
+    opts = function(_, opts)
+      opts.extensions = opts.extensions or {}
+      opts.extensions["ui-select"] = { require("telescope.themes").get_dropdown() }
+      return opts
+    end,
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension "ui-select"
+    end,
+  },
+
+  {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = "markdown",
     cmd = { "RenderMarkdown" },
